3c2b43165d20e82a710a4f97306774842a547c04
Joshua Smith <joshua.l.smith@nordstrom.com>
2013-10-02 17:45:40 -0700

    Cleaned up Josh_ReverseList.hs a bit.

--- Josh_ReverseList.hs
+++ Josh_ReverseList.hs
@@ -1,7 +1,6 @@
 -- Josh_ReverseList.hs
 -- Given a list of elements, returns the list in reverse.
 
-
 -- This is the kind of efficiency that earns me the medium dollars.
 reverseListEZ :: [a] -> [a]
 reverseListEZ list = reverse list
@@ -11,6 +10,8 @@ reverseList :: [a] -> [a]
 -- Given an empty list, return an empty list.
 -- This pattern match serves as the recursion terminator.
 reverseList [] = []
+reverseList (x:xs) = reverseList xs ++ [x]
+
 {-
 (x:xs) binds the first element in the list to x, and the rest of the list to xs.
 In other words, x = head, xs = tail.
@@ -22,4 +23,3 @@ which triggers the first pattern match and terminates recursion.
 The output of each recursion is the tail (reversed),
 to which the head (at the level that initiated the recursive call) is appended.
 -}
-reverseList (x:xs) = reverseList xs ++ [x]
