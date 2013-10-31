e8ef2d9d361641b12cb373986010f031ec113539
nils janson <nils.janson@nordstrom.com>
2013-10-31 12:02:14 -0700

    updated week3

--- NIls_week3.hs
+++ NIls_week3.hs
@@ -1,4 +1,6 @@
 import qualified Data.Map as Map
+import qualified Data.Set as Set
+import qualified Data.Maybe as Maybe
 
 --data MyValues = StringValue String | MapValue (Map.Map String MyValues) deriving (Show)
 --stringValue :: MyValues -> String
@@ -10,6 +12,29 @@ import qualified Data.Map as Map
 data MyValues = StringValue {valueString :: String} | MapValue {valueMap :: (Map.Map String MyValues)} deriving (Show)
 
 -- Find in hash
---findInHash :: (Eq k, Hashable k, MyValues a) => k -> Map.Map k a -> Maybe v
---findInHash _  h
---	| Map.Map.null h = Nothing
+--findInMap :: (Eq k) => k -> Map.Map k a -> Maybe v
+--findInMap _ h
+--	| Map.null h = Nothing;
+--pullOutMap :: MapValue -> (Map.Map String MyValues)
+--pullOutMap () 
+
+someStringValue = StringValue "theValue"
+singelton = 	Map.singleton "one" someStringValue
+someStringValue1 = StringValue "theValue1"
+two = MapValue singelton	
+mapmap = 	Map.fromList  [("one1", someStringValue1),("2", two)]
+mapValueMap = MapValue mapmap
+
+
+findInMap :: String -> MyValues -> Maybe String
+findInMap _ (StringValue v) = Just v
+findInMap kk	 (MapValue  m) 	
+	| Map.member kk m = Just $ valueString $ Maybe.fromJust $ Map.lookup kk m
+	| otherwise = overList kk $ Map.toList m
+	where overList kkk ((k3, v3):rest) = findInMap kkk v3
+--	where overList kkk ((k3, v3):rest) = findInMap kkk $ MapValue $ Map.fromList rest
+
+
+
+
+
